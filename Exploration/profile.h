7a9cbdf3644e10d296b4995a1b3e216a06319126
dhusdstudent <u0793491@umail.utah.edu>
2026-04-20 16:56:57 -0600

    Update profile.h

--- Exploration/profile.h
+++ Exploration/profile.h
@@ -1,9 +1,23 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+#include <QString>
+
 class Profile
 {
+    
 public:
+    QString firstName;
+    QString lastName;
+    QString birthday_day;
+    QString birthday_month;
+    QString birthday_year;
+    QString gender;
+    QString username;
+    QString password;
+    QString profileImagePath;
+    int bestScore;
+    
     Profile();
 };
 
